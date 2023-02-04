41d84254780809c5dfe8724f7af1d504d616db2c
coder <103135051+uzxakker@users.noreply.github.com>
2023-02-04 12:16:36 +0500

    push

--- src/router/styled-index.js
+++ src/router/styled-index.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   padding: 30px 0;
-  padding-top: 150px;
+  padding-top: 100px;
   @media only screen and (max-width: 425px) {
-  padding-top: 85px;
+    padding-top: 50px;
   }
-`
+`;
