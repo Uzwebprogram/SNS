b24372962ab40fb4032a07fbbbe8b6b7d645cd15
uzxakker <103135051+uzxakker@users.noreply.github.com>
2023-06-21 21:52:56 +0500

    push

--- src/components/uslugi-rating/types-rating/index.jsx
+++ src/components/uslugi-rating/types-rating/index.jsx
@@ -1,14 +1,74 @@
 import React from 'react'
+import { useState } from 'react';
 import styles from './style.module.css'
 import { WrapperContainer } from '../../../style-App'
 import { Col, Row } from 'react-grid-system'
 import { useTranslation } from 'react-i18next'
+import RatingType1Modal from './raiting-type1/index'
+import RatingType3Modal from './raiting-type3/index'
+import RatingType4Modal from './raiting-type4/index'
+
+import RatingType2Modal from './raiting-type2/index';
 
 const TypesRatings = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation()
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
+  const [isModalOpen3, setIsModalOpen3] = useState(false);
+
+  // rating type modal 1 function
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  // rating type modal 1 function
+  // rating type modal 2 function
+  const showModal1 = () => {
+    setIsModalOpen1(true);
+  };
+  const handleOk1 = () => {
+    setIsModalOpen1(false);
+  };
+  const handleCancel1 = () => {
+    setIsModalOpen1(false);
+  };
+  // rating type modal 2 function
+  // rating type modal 3 function
+  const showModal2 = () => {
+    setIsModalOpen2(true);
+  };
+  const handleOk2 = () => {
+    setIsModalOpen2(false);
+  };
+  const handleCancel2 = () => {
+    setIsModalOpen2(false);
+  };
+  // rating type modal 3 function
+  // rating type modal 4 function
+  const showModal3 = () => {
+    setIsModalOpen3(true);
+  };
+  const handleOk3 = () => {
+    setIsModalOpen3(false);
+  };
+  const handleCancel3 = () => {
+    setIsModalOpen3(false);
+  };
+  // rating type modal 4 function
   return (
     <>
       <div className={styles.types_rating_section}>
+        <RatingType1Modal handleOk={handleOk} handleCancel={handleCancel} isModalOpen={isModalOpen} />
+        <RatingType2Modal handleOk1={handleOk1} handleCancel1={handleCancel1} isModalOpen1={isModalOpen1}/>
+        <RatingType3Modal handleOk2={handleOk2} handleCancel2={handleCancel2} isModalOpen2={isModalOpen2} />
+        <RatingType4Modal handleOk3={handleOk3} handleCancel3={handleCancel3} isModalOpen3={isModalOpen3} />
         <WrapperContainer>
           <Row className={styles.types_rating_row}>
             <Col className={styles.types_rating_col} lg={6} md={12} sm={12}>
@@ -18,16 +78,16 @@ const TypesRatings = () => {
             <Col className={styles.types_rating_col} lg={6} md={12} sm={12}>
               <h4>{t("UslugiRating.3")}</h4>
               <div className={styles.types_rating_link_box}>
-                <button type='button'>{t("UslugiRating.4")}</button>
+                <button onClick={showModal} type='button'>{t("UslugiRating.4")}</button>
               </div>
               <div className={styles.types_rating_link_box}>
-                <button type='button'>{t("UslugiRating.5")}</button>
+                <button onClick={showModal1} type='button'>{t("UslugiRating.5")}</button>
               </div>
               <div className={styles.types_rating_link_box}>
-                <button type='button'>{t("UslugiRating.6")}</button>
+                <button onClick={showModal2} type='button'>{t("UslugiRating.6")}</button>
               </div>
               <div className={styles.types_rating_link_box}>
-                <button type='button'>{t("UslugiRating.7")}</button>
+                <button onClick={showModal3} type='button'>{t("UslugiRating.7")}</button>
               </div>
             </Col>
           </Row>
---
+++ src/components/uslugi-rating/types-rating/raiting-type4/index.jsx
@@ -0,0 +1,172 @@
+import React from 'react'
+import { Modal } from 'antd';
+import { useTranslation } from 'react-i18next';
+import styles from './style.module.css'
+import { Row, Col } from 'react-grid-system'
+import { Image } from 'antd';
+import './style.css'
+
+import ModalType1 from '../../../../assets/modal/modal1.png'
+import ModalType2 from '../../../../assets/modal/modal1.2.png'
+import ModalType3 from '../../../../assets/modal/modal1.3.png'
+import ModatType4 from '../../../../assets/modal/modal1.4.png'
+
+
+
+
+
+const RatingType4Modal = ({ handleOk3, handleCancel3, isModalOpen3 }) => {
+    const { t } = useTranslation()
+    return (
+        <>
+            <Modal width={1000} footer={false} open={isModalOpen3} onOk={handleOk3} onCancel={handleCancel3}>
+                <div className={styles.modal_title_box}>
+                    <h4>{t("UslugiRating.RatingType4.0")}</h4>
+                    <hr />
+                </div>
+                <div className={styles.modal_content_wrapp}>
+                    <p>{t("UslugiRating.RatingType4.1")}</p>
+                    <p>{t("UslugiRating.RatingType1.2")}</p>
+                    <ul>
+
+                        <li>{t("UslugiRating.RatingType1.5")}</li>
+                        <li>{t("UslugiRating.RatingType1.6")}</li>
+                    </ul>
+                    <p>{t("UslugiRating.RatingType1.7")}</p>
+
+                    {/* <div className={styles.modal_card_wrap}>
+                        <h4>{t("UslugiRating.RatingType1.8")}</h4>
+                        <Row className={styles.modal_card_row}>
+                            <Col className={styles.modal_card_col} lg={4} md={12} sm={12}>
+                                <img src={ModalType1} alt="" />
+                                <p>{t("UslugiRating.RatingType1.9")}</p>
+                            </Col>
+                            <Col className={styles.modal_card_col} lg={4} md={12} sm={12}>
+                                <img src={ModalType2} alt="" />
+                                <p>{t("UslugiRating.RatingType1.10")}</p>
+                            </Col>
+                            <Col className={styles.modal_card_col} lg={4} md={12} sm={12}>
+                                <img src={ModalType3} alt="" />
+                                <p>{t("UslugiRating.RatingType1.11")}</p>
+                            </Col>
+                        </Row>
+                    </div> */}
+
+                    {/* <div className={styles.struct_wrap}>
+                        <h4 className={styles.struct_wrap_title}>{t("UslugiRating.RatingType1.12")}</h4>
+                        <Row className={styles.struct_wrap_row}>
+                            <Col className={styles.struct_wrap_col} lg={6} md={12} sm={12}>
+                                <h4>{t("UslugiRating.RatingType1.13")}</h4>
+                                <div className={styles.struct_wrap_col_item}>
+                                    <ul>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.14")}</p>
+                                        </li>
+                                        <hr />
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.15")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.16")}</p>
+                                        </li>
+                                        <hr />
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.17")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.18")}</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Col>
+                            <Col className={styles.struct_wrap_col} lg={6} md={12} sm={12}>
+                                <h4>{t("UslugiRating.RatingType1.19")}</h4>
+                                <div className={styles.struct_wrap_col_item}>
+                                    <ul>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.20")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.21")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.22")}</p>
+                                        </li>
+
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.24")}</p>
+                                        </li>
+                                        <hr />
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.25")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.26")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.27")}</p>
+                                        </li>
+                                        <li>
+                                            <i class='bx bxs-right-arrow-alt'></i>
+                                            <p>{t("UslugiRating.RatingType1.28")}</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div> */}
+
+                    <div className={styles.rating_scale_wrap}>
+                        <h4>{t("UslugiRating.RatingType1.29")}</h4>
+                        <p>{t("UslugiRating.RatingType1.30")}</p>
+                        <Image
+                            width="100%"
+                            height="100%"
+                            src={ModatType4}
+                        />
+                    </div>
+
+                    <div className={styles.prognos}>
+                        <h4>{t("UslugiRating.RatingType1.31")}</h4>
+                        <p>{t("UslugiRating.RatingType1.32")}</p>
+
+                        <ul>
+                            <li>
+                                <span>{t("UslugiRating.RatingType1.33")}</span>
+                                <span>{t("UslugiRating.RatingType1.34")}</span>
+                            </li>
+                            <li>
+                                <span>{t("UslugiRating.RatingType1.35")}</span>
+                                <span>{t("UslugiRating.RatingType1.36")}</span>
+                            </li>
+                            <li>
+                                <span>{t("UslugiRating.RatingType1.37")}</span>
+                                <span>{t("UslugiRating.RatingType1.38")}</span>
+                            </li>
+                            <li>
+                                <span>{t("UslugiRating.RatingType1.39")}</span>
+                                <span>{t("UslugiRating.RatingType1.40")}</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </Modal>
+        </>
+    )
+}
+
+
+export default RatingType4Modal
